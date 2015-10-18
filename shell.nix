68bfbedf9273afe853bbd843422ba2a6dc56d700
Nikita Karetnikov <nikita@karetnikov.org>
2015-10-19 01:21:14 +0300

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,19 +1,35 @@
-with (import <nixpkgs> {}).pkgs;
-let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, base, blaze-markup, cabal-install, git, semigroups
-             , stdenv, text, tasty, tasty-hunit, tasty-quickcheck }:
-             mkDerivation {
-               pname = "titlecase";
-               version = "0.1.0.1";
-               src = ./.;
-               buildDepends = [ base blaze-markup semigroups text ];
-               testDepends = [
-                 base semigroups tasty tasty-hunit tasty-quickcheck text
-               ];
-               buildTools = [ cabal-install git ];
-               homepage = "https://github.com/nkaretnikov/titlecase";
-               description = "Convert English words to title case";
-               license = stdenv.lib.licenses.bsd3;
-             }) {};
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, blaze-markup, cabal-install, semigroups
+      , stdenv, tasty , tasty-hunit, tasty-quickcheck, text
+      }:
+      mkDerivation {
+        pname = "titlecase";
+        version = "0.1.0.1";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [ base blaze-markup semigroups text ];
+        executableHaskellDepends = [ base blaze-markup text ];
+        testHaskellDepends = [
+          base semigroups tasty tasty-hunit tasty-quickcheck text
+        ];
+        buildTools = [ cabal-install ];
+        homepage = "https://github.com/nkaretnikov/titlecase";
+        description = "Convert English words to title case";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
 in
-  pkg.env
+
+  if pkgs.lib.inNixShell then drv.env else drv
