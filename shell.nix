56cbe90476998bf7c50bd767f02f97a3532bc2ed
Nikita Karetnikov <nikita@karetnikov.org>
2015-11-08 18:03:53 +0300

    Version bump

--- shell.nix
+++ shell.nix
@@ -9,7 +9,7 @@ let
       }:
       mkDerivation {
         pname = "titlecase";
-        version = "0.1.0.1";
+        version = "0.1.0.2";
         src = ./.;
         isLibrary = true;
         isExecutable = true;
